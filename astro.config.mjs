040784d4ce9e4afa058d7775915fbdbe80681ed6
Daniel-Jaekel <148904595+Daniel-Jaekel@users.noreply.github.com>
2024-06-09 00:28:15 +0200

    Finish tutorial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://github.com/Daniel-Jaekel',
-    base: '/github-page-test-2',
+  site: 'https://github.com/Daniel-Jaekel',
+  base: '/github-page-test-2',
+  integrations: [preact()]
 });
